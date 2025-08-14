cae04bd09ddb8dd4828ca65b58693b49c3c63202
zgozh <13610537014@163.com>
2025-08-14 23:00:07 +0800

    添加枪械

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,13 +2,19 @@ class_name Player
 extends CharacterBody2D
 
 @onready var player_input_synchronizer_component: PlayerInputSynchronizerComponent = $PlayerInputSynchronizerComponent
+@onready var weapon_root: Node2D = $WeaponRoot
 
 var input_multiplayer_authority: int
 
 func _ready() -> void:
 	player_input_synchronizer_component.set_multiplayer_authority(input_multiplayer_authority)
-	set_process(is_multiplayer_authority())
+
 
 func _process(delta: float) -> void:
-	velocity = player_input_synchronizer_component.movement_vector * 100
-	move_and_slide()
+	var aim_position = weapon_root.global_position \
+		+ player_input_synchronizer_component.aim_vector
+	weapon_root.look_at(aim_position)
+	
+	if is_multiplayer_authority():
+		velocity = player_input_synchronizer_component.movement_vector * 100
+		move_and_slide()
